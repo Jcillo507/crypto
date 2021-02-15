c072bfbd1a6c33b449347dd17c87d6f1ecad91c9
Joe Cillo <JCillo507@yahoo.com>
2021-02-15 14:53:16 -0500

    urlified text

--- client/src/components/CoinDetail/index.jsx
+++ client/src/components/CoinDetail/index.jsx
@@ -1,24 +1,24 @@
 import React from "react";
 
 const CoinDetail = (props) => {
-  const {profile}= props.details
-  if(props.details.profile){
-
-    console.log(
-      profile
-    );
-  }
-  const display=()=>{
+  const { profile } = props.details;
+  const turnIntoLink = (text) => {
+    const urlFilter = /\[([^\][]*)]\(((?:https?|ftps?|file):\/\/[^()]*)\)/gi;
+    return text.replace(urlFilter, '<a href="$2">$1</a>');
+  };
+  const display = () => {
     return (
       <div className="">
-        <p>
-          Consensus Mechanism:
-          {" " +
-            profile.economics.consensus_and_emission.consensus
-              .general_consensus_mechanism}
-        </p>
-        <p>Launch Details: {profile.economics.launch.general.launch_details}</p>
-        <p>Launch style: {profile.economics.launch.general.launch_style}</p>
+        <p>Sector: {profile.general.overview.sector}</p>
+        <p>Tagline: {profile.general.overview.tagline}</p>
+        <div>
+          Project Details:
+          <span
+            dangerouslySetInnerHTML={{
+              __html: turnIntoLink(profile.general.overview.project_details),
+            }}
+          ></span>
+        </div>
         <p>
           Genesis Date:{" "}
           {profile.economics.launch.initial_distribution.genesis_block_date}
@@ -28,21 +28,39 @@ const CoinDetail = (props) => {
           {profile.economics.launch.initial_distribution.initial_supply}
         </p>
         <p>
-          Background Details:{profile.general.background.background_details}
+          Consensus Mechanism:
+          {" " +
+            profile.economics.consensus_and_emission.consensus
+              .general_consensus_mechanism}
         </p>
-        <p>Project Details: {profile.general.overview.project_details}</p>
-        <p>Sector: {profile.general.overview.sector}</p>
-        <p>Tagline: {profile.general.overview.tagline}</p>
+        <p>
+          Launch style:
+          {profile.economics.launch.general.launch_style}
+        </p>
+        <div>
+          Launch Details:{" "}
+          <span
+            dangerouslySetInnerHTML={{
+              __html: turnIntoLink(
+                profile.economics.launch.general.launch_details
+              ),
+            }}
+          ></span>
+        </div>
+        <div>
+          Background Details:
+          <span
+            dangerouslySetInnerHTML={{
+              __html: turnIntoLink(
+                profile.general.background.background_details
+              ),
+            }}
+          ></span>
+        </div>
       </div>
     );
-  }
-  return (
-    <section>
-   {
-     profile?display():'Loading'
-   }
-    </section>
-  );
+  };
+  return <section>{profile ? display() : "Loading"}</section>;
 };
 
 export default CoinDetail;
